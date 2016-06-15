bc09a5a2be43cd80bba154569ff5e9cf52c1a5a4
Michal Glajc [poznet] <pozycjoner.net@gmail.com>
2016-06-15 21:52:59 +0000

    view

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-antyspam
+antispam
 ========
 
 A Symfony project created on June 15, 2016, 10:08 pm.
--- src/AntispamBundle/Controller/DefaultController.php
+++ src/AntispamBundle/Controller/DefaultController.php
@@ -4,14 +4,28 @@ namespace AntispamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @Template
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('AntispamBundle:Default:index.html.twig');
+
+        if($request->getMethod()=='POST'){
+
+        }
+        $config=array();
+        $config['email']=$this->get('configuration')->get('email');
+        $config['password']=$this->get('configuration')->get('password');
+        $config['login']=$this->get('configuration')->get('login');
+        $config['imap']=$this->get('configuration')->get('imap');
+
+
+        return array('config'=>$config);
     }
 }

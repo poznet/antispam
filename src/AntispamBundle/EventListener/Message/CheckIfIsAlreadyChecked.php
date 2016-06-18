bf2dfcd09f03cde5533326ababf12cbc63dd01d7
Michal Glajc [poznet] <pozycjoner.net@gmail.com>
2016-06-18 12:48:04 +0200

    works :   - check if message was already checked   - checki if message is whitelised

---
+++ src/AntispamBundle/EventListener/Message/CheckIfIsAlreadyChecked.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joseph
+ * Date: 17.06.16
+ * Time: 23:42
+ */
+
+namespace AntispamBundle\EventListener\Message;
+
+
+use AntispamBundle\Event\MessageEvent;
+use AntispamBundle\Services\MessageService;
+use Ddeboer\Imap\Exception\Exception;
+use Doctrine\ORM\EntityManager;
+
+class CheckIfIsAlreadyChecked
+{
+
+    private $ms;
+
+    public function __construct(MessageService $ms)
+    {
+        $this->ms=$ms;
+    }
+
+    public function check(MessageEvent $event){
+        $msg=$event->getMessage();
+        if($this->ms->isChecked($msg->getId())) {
+            $event->stopPropagation();
+        }
+
+    }
+
+}
